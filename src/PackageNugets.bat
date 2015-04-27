d7947210b90c7e1de0fdafcadd6df6827a2ac3ed
Ben Hanson <benhans@microsoft.com>
2015-04-27 15:27:24 -0700

    Got the NuGet packing "working".  Up to 0.0.4-alpha now.

--- src/PackageNugets.bat
+++ src/PackageNugets.bat
@@ -1,2 +1,2 @@
-.nuget\nuget.exe pack .\Cloud.Storage\Cloud.Storage.nuspec
-.nuget\nuget.exe pack .\Cloud.Storage.Azure\Cloud.Storage.Azure.nuspec
+.nuget\nuget.exe pack .\Cloud.Storage\Cloud.Storage.csproj
+.nuget\nuget.exe pack .\Cloud.Storage.Azure\Cloud.Storage.Azure.csproj
